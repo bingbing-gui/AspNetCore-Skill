26f721c62c689f6f42ef29daec1e746d451d64ff
bingbing gui <bingbing.gui@outlook.com>
2020-07-11 19:00:25 +0900

    Add project files.

---
+++ MediatR.FrameWork/MediatR.FrameWork/Handlers/GetOrderByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR.FrameWork.Practice.Queries;
+using MediatR.FrameWork.Practice.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediatR.FrameWork.Practice.Handlers
+{
+    public class GetOrderByIdHandler : IRequestHandler<GetOrderByIdQuery, Order>
+    {
+        private readonly IOrderService _orderService;
+        public GetOrderByIdHandler(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        public async Task<Order> Handle(GetOrderByIdQuery request, CancellationToken cancellationToken)
+        {
+            var order = await _orderService.GetSingleOrder(request.OrderId);
+            return order;
+        }
+    }
+}
---
+++ MediatR.FrameWork/MediatR.FrameWork/Startup.cs
@@ -0,0 +1,59 @@
+using MediatR.FrameWork.Practice.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MediatR.FrameWork
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddMediatR(typeof(Startup));
+            services.AddScoped<IOrderService, OrderService>();
+            //Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            #region Swagger
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "OrderService");
+            });
+            #endregion
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
