32cef5587b2fabcadea45ce1faa7db4423ff71c3
bingbing gui <bingbing.gui@outlook.com>
2020-09-06 21:39:06 +0900

    add jwt  practice project

---
+++ JWT/AspNetCore.JWT.Practice/Controllers/LoginController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetCore.JWT.Practice.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AspNetCore.JWT.Practice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private List<User> users = new List<User>
+        {
+        new User { FullName = "zhangsan", UserName = "admin", Password = "1234", UserRole = "Admin"},
+        new User { FullName = "wangwu", UserName = "user", Password = "1234", UserRole = "User" }
+        };
+
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(IConfiguration config, ILogger<LoginController> logger)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody] User loginuser)
+        {
+            IActionResult response = Unauthorized();
+            User user = users.SingleOrDefault(
+                            x => x.UserName == loginuser.UserName
+                            && x.Password == loginuser.Password);
+            if (user != null)
+            {
+                var tokenString = GenerateJWTToken(user);
+                response = Ok(new
+                {
+                    token = tokenString,
+                    userDetails = user,
+                });
+            }
+            return response;
+        }
+        string GenerateJWTToken(User userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"]));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,userInfo.UserName),
+                new Claim("fullName",userInfo.FullName.ToString()),
+                new Claim("role",userInfo.UserRole),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: credentials
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ JWT/AspNetCore.JWT.Practice/Startup.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetCore.JWT.Practice.Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+namespace AspNetCore.JWT.Practice
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            //1.注册JWT authentication scheme
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        //指定是否验证Issuer
+                        ValidateIssuer = true,
+                        //是否验证Audience在token 验证期间
+                        ValidateAudience = true,
+                        //获取和设定一个boolen 是否验证声明周期在整个token 验证期间
+                        ValidateLifetime = true,
+                        //获取或设置一个布尔值，该布尔值控制是否调用对签名SecurityKey进行验证
+                        ValidateIssuerSigningKey = true,
+                        //是一个字符串
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        //是一个字符串
+                        ValidAudience = Configuration["Jwt:Audience"],
+                        //IssuerSigningKey是用于验证传入的JWT Token。通过在此处指SecretKey
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:SecretKey"])),
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+            services.AddAuthorization(config =>
+            {
+                config.AddPolicy(Policies.Admin, Policies.AdminPolicy());
+                config.AddPolicy(Policies.User, Policies.UserPolicy());
+            });
+            //Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspNetCore.JWT", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"
+                        JWT Authorization header using the Bearer scheme.
+                        Enter 'Bearer' [space] and then your token in the text input below.
+                        Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                      }
+                    });
+                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                //c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            #region Swagger
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "LoginService");
+            });
+            #endregion
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            //添加UseAuthentication
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
