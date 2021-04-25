8e10eb82cf1e9e887249bdd1d059c866ef5b9a3e
bingbing gui <bingbing.gui@outlook.com>
2021-04-25 15:37:58 +0900

    DI LiftTime

---
+++ AspNetCore.DependencyInjection/AspNetCore.DependencyInjection.Lifetime.Practice/Service/Operation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AspNetCore.DependencyInjection.Lifetime.Practice
+{
+    public class Operation : IOperationTransient, IOperationScoped, IOperationSingleton
+    {
+        public Operation()
+        {
+            OperationId = Guid.NewGuid().ToString();
+        }
+        public string OperationId { get; }
+    }
+}
