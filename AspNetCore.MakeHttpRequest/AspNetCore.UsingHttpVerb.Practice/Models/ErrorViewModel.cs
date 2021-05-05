95753857492b9fad3139ac959bdb681528e8f91c
bingbing gui <bingbing.gui@outlook.com>
2021-05-05 18:54:05 +0900

    使用HttpClient 发送POST GET PUT DELETE 请求

---
+++ AspNetCore.MakeHttpRequest/AspNetCore.UsingHttpVerb.Practice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCore.UsingHttpVerb.Practice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
