c92777f4cc35baf61cc4e290f19598ca98ee9df5
bingbing gui <bingbing.gui@outlook.com>
2022-04-12 00:03:41 +0800

    Four ways to dispose IDisposables in ASP.NET Core

---
+++ 02-AspNetCore.DependencyInjection/AspNetCore6.FourWaysToDisposeIDisposables/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using AspNetCore6.FourWaysToDisposeIDisposables.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+
+namespace AspNetCore6.FourWaysToDisposeIDisposables.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        readonly IDisposable _disposable;
+
+        public HomeController(
+        TransientCreatedByContainer transient,
+        ScopedCreatedByFactory scoped,
+        SingletonCreatedByContainer createdByContainer,
+        SingletonAddedManually manually,
+        ILogger<HomeController> logger)
+        {
+            //_disposable = new MyDisposable();
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //HttpContext.Response.RegisterForDispose(_disposable);
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            using (var disposal = new MyDisposable())
+            {
+
+            }
+            MyDisposable myObject=null;
+            try
+            {
+                myObject = new MyDisposable();
+            }
+            finally
+            {
+                myObject?.Dispose();
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 02-AspNetCore.DependencyInjection/AspNetCore6.FourWaysToDisposeIDisposables/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCore6.FourWaysToDisposeIDisposables.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
