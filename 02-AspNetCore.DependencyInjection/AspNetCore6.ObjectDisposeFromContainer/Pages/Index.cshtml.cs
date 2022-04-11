25fe4ab36d6eacf6eba56dbc321c94a1f2799e29
bingbing gui <bingbing.gui@outlook.com>
2022-04-11 23:01:42 +0800

    dispose 容器内的对象

---
+++ 02-AspNetCore.DependencyInjection/AspNetCore6.ObjectDisposeFromContainer/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCore6.ObjectDisposeFromContainer.Pages
+{
+    public class IndexModel : PageModel
+    {       
+        private readonly Service1 _service1;
+        private readonly Service2 _service2;
+        private readonly IService3 _service3;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(
+            Service1 service1,
+            Service2 service2,
+            IService3 service3,
+            ILogger<IndexModel> logger)
+        {
+            _service1 = service1;
+            _service2 = service2;
+            _service3 = service3;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _service1.Write("Service1.OnGet()");
+            _service2.Write("Service2.OnGet()");
+            _service3.Write("Service3.OnGet()");
+        }
+    }
+}
---
+++ 02-AspNetCore.DependencyInjection/AspNetCore6.ObjectDisposeFromContainer/Services/DisposeService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore6.ObjectDisposeFromContainer
+{    
+    public class Service1 : IDisposable
+    {
+        private bool _disposed;
+        public void Write(string message)
+        {
+            Console.WriteLine($"********************************************************************");
+            Console.WriteLine($"Service1:{message}");
+            Console.WriteLine($"********************************************************************");
+        }
+        public void Dispose()
+        {
+            if (_disposed)
+                return;
+            Console.WriteLine($"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            Console.WriteLine("Service1.Dispose");
+            Console.WriteLine($"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            _disposed = true;
+        }
+    }
+    public class Service2 : IDisposable
+    {
+        private bool _disposed;
+        public void Write(string message)
+        {
+            Console.WriteLine($"********************************************************************");
+            Console.WriteLine($"Service2:{message}");
+            Console.WriteLine($"********************************************************************");
+        }
+        public void Dispose()
+        {
+            if (_disposed)
+                return;
+            Console.WriteLine($"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            Console.WriteLine("Service2.Dispose");
+            Console.WriteLine($"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            _disposed = true;
+        }
+    }
+    public interface IService3
+    {
+        public void Write(string message);
+    }
+    public class Service3 : IService3, IDisposable
+    {
+        private bool _disposed;
+
+        public string MyKey { get; }
+        public Service3(string myKey)
+        {
+            MyKey = myKey;
+        }
+        public void Write(string message)
+        {
+            Console.WriteLine($"********************************************************************");
+            Console.WriteLine($"Service3: {message}, MyKey = {MyKey}");
+            Console.WriteLine($"********************************************************************");
+        }
+        public void Dispose()
+        {
+            if (_disposed)
+                return;
+            Console.WriteLine($"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            Console.WriteLine("Service3.Dispose");
+            Console.WriteLine($"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            _disposed = true;
+        }
+    }
+}
