4bceb9e8e3796ab0a9e938fe310408df8f578f29
bingbing gui <bingbing.gui@outlook.com>
2022-05-30 22:58:09 +0800

    添加模型绑定

---
+++ MVC/01-AspNetCore.ModelBinding/AspNetCore.ModelBinding/Controllers/PetsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.ModelBinding.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class PetsController : ControllerBase
+{
+    [HttpGet("{id}")]
+    public ActionResult<Pet> GetById(int id, bool dogsOnly)
+    {
+        return NoContent();
+    }
+    [HttpPost("Create")]
+    public ActionResult<Pet> Create([FromBody] Pet pet)
+    {
+        return NoContent();
+    }
+    [HttpPost("CreateList")]
+    public ActionResult<Pet> CreateList([FromBody] List<Pet> pets)
+    {
+        return NoContent();
+    }
+    [HttpPost("Edit")]
+    public ActionResult<Instructor> Edit([FromBody] Instructor instructor)
+    {
+        return NoContent();
+    }
+    
+}
