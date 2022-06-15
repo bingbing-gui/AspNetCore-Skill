dd88657090fc344167131a72778d9a5d649ee729
bingbing gui <bingbing.gui@outlook.com>
2022-06-15 23:28:35 +0800

    新增Identity Register and Login Method

---
+++ SecurityIdentity/AspNetCore.Authentication/AspNetCore.Identity/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Identity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public HomeController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            var identityUser = await _userManager.FindByNameAsync(username);
+            if (identityUser != null)
+            {
+                var signInResult=await _signInManager.PasswordSignInAsync(identityUser, password, false, false);
+                if (signInResult.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            IdentityUser identityUser = new IdentityUser()
+            {
+                UserName = username,
+                Email = ""
+            };
+
+            var identityResult = await _userManager.CreateAsync(identityUser, password);
+            if (identityResult.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- SecurityIdentity/AspNetCore.Authentication/AspNetCore.Identity/Program.cs
+++ SecurityIdentity/AspNetCore.Authentication/AspNetCore.Identity/Program.cs
@@ -1,6 +1,68 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+﻿using AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+//注册DBContext
+builder.Services.AddDbContext<ApplicationDbContext>(optionsAction =>
+{
+    optionsAction.UseSqlServer(connection);
+});
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+//添加Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(identityOption =>
+{
+    identityOption.Password.RequiredLength = 4;
+    identityOption.Password.RequireDigit = false;
+    identityOption.Password.RequireNonAlphanumeric = false;
+    identityOption.Password.RequireUppercase = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(configure =>
+{
+    configure.Cookie.Name = "Identity.Cookie";
+    configure.LoginPath = "/Home/Login";
+    configure.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+    configure.SlidingExpiration = true;
+});
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+/*初始化数据库*/
+
+using (var serviceScope = app.Services.CreateScope())
+{
+    serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.EnsureCreated();
+}
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
