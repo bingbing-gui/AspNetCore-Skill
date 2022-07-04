0d8bdba2f78bbe074e3cd77ab8a25756b7078ab7
bingbing gui <bingbing.gui@outlook.com>
2022-07-04 23:43:27 +0800

    那件ASP.NET Core AntiForgery

--- SecurityIdentity/AspNetCore.Authentication/AspNetCore.Cookie/Pages/Account/Login.cshtml.cs
+++ SecurityIdentity/AspNetCore.Authentication/AspNetCore.Cookie/Pages/Account/Login.cshtml.cs
@@ -55,7 +55,28 @@ namespace AspNetCore.Cookie.Page.Account
                 var claimsPrincipal = new ClaimsPrincipal(claimIdentity);
 
                 var authenticationProperties = new AuthenticationProperties()
-                { };
+                {
+                    //AllowRefresh = <bool>,
+                    // Refreshing the authentication session should be allowed.
+
+                    //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                    // The time at which the authentication ticket expires. A 
+                    // value set here overrides the ExpireTimeSpan option of 
+                    // CookieAuthenticationOptions set with AddCookie.
+
+                    //IsPersistent = true,
+                    // Whether the authentication session is persisted across 
+                    // multiple requests. When used with cookies, controls
+                    // whether the cookie's lifetime is absolute (matching the
+                    // lifetime of the authentication ticket) or session-based.
+
+                    //IssuedUtc = <DateTimeOffset>,
+                    // The time at which the authentication ticket was issued.
+
+                    //RedirectUri = <string>
+                    // The full path or absolute URI to be used as an http 
+                    // redirect response value.
+                };
 
                 await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
                     claimsPrincipal,
