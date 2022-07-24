88845a569839bfe22bc3cd61750837894209019a
bingbing gui <bingbing.gui@outlook.com>
2022-07-24 23:34:27 +0800

    Add MVC Project

---
+++ WebApps/MVC/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["numTimes"] = numTimes;
+            return View();
+            //return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+        // GET: /HelloWorld/Welcome2/ 
+        //Id 为RouteData上
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome2(string name, int id = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {id}");
+        }
+    }
+}
---
+++ WebApps/MVC/MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movie { get; set; } = default!;
+    }
+}
