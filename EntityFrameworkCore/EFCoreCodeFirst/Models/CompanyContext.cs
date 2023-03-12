049ccfeeac2b8498e1153f500c97bd5138f3a405
bingbing gui <bingbing.gui@outlook.com>
2023-03-12 17:36:55 +0800

    添加EFCoreDbContext和EFCoreCodeFirst Project

---
+++ EntityFrameworkCore/EFCoreCodeFirst/Models/CompanyContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace EFCoreCodeFirst.Models
+{
+    public class CompanyContext:DbContext
+    {
+        public CompanyContext(DbContextOptions<CompanyContext> dbContextOptions)
+        :base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Information> Information { get; set; }
+    }
+}
