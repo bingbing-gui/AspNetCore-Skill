502febf1679d4ccf2743bffaebc76698ec869d32
bingbing gui <bingbing.gui@outlook.com>
2023-04-20 12:03:41 +0900

    Entity Framework Core-约定

---
+++ EntityFrameworkCore/EFCoreConventions/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCoreConventions.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Employee> Employee { get; set; }
+    }
+}
