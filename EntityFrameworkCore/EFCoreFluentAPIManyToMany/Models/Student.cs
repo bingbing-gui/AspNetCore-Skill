aec8c1ec86229f490876e8d79f76d776e88c7abb
bingbing.gui <bingbing@DESKTOP-K7ELFM9>
2023-05-02 15:04:43 +0900

    EF Core-使用Fluent API配置多对多关系

---
+++ EntityFrameworkCore/EFCoreFluentAPIManyToMany/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCoreFluentAPIManyToMany.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Teacher> Teacher { get; set; } //collection navigation property
+    }
+}
