6c3d3f6beab14401ff87ad1a69b1db0d106e71fa
bingbing.gui <bingbing@DESKTOP-K7ELFM9>
2023-05-02 17:31:43 +0900

    Entity Framework Core-使用FromSqlRaw() 执行原生SQL查询

---
+++ EntityFrameworkCore/EFCoreExecuteRawSql/Models/EmployeeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreExecuteRawSql.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
