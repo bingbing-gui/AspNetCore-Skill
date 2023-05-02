83652fdaf4cefc068f1fc210a334010302451bde
bingbing.gui <bingbing@DESKTOP-K7ELFM9>
2023-05-02 14:18:01 +0900

    EF Core-Fluent API-一对一关系

---
+++ EntityFrameworkCore/EFCoreFluentAPIOneToOne/Models/CountryContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace EFCoreFluentAPIOneToOne.Models
+{
+    public class CountryContext : DbContext
+    {
+        public DbSet<City> City { get; set; }
+        public DbSet<CityInformation> CityInformation { get; set; }
+        public CountryContext(DbContextOptions<CountryContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        //Write Fluent API configurations here​
+            modelBuilder.Entity<City>()
+                        .HasOne(e => e.CityInformation)
+                        .WithOne(e => e.City)
+                        .HasForeignKey<City>(e => e.CityInformationId);
+        }
+    }
+}
