d4c76b472e2651d6708267b967787c7f6ee76571
bingbing.gui <bingbing@DESKTOP-K7ELFM9>
2023-06-30 13:17:36 +0900

    ASP.NET Core Views

---
+++ Fundamentals/AspNetCore.Views/AspNetCore.Views/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using AspNetCore.Views.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Views.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(int id, string name)
+        {
+            string welcomeMessage = $"欢迎员工: {name} 编号: {id}";
+            return View((object)welcomeMessage);
+        }
+        [HttpGet]
+        public IActionResult Detail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Detail(int id, string name)
+        {
+            
+            var employee = new Employee();
+            employee.Id = id;
+            employee.Name = name;
+            employee.Salary = 1000;
+            employee.Designation = "Manager";
+            employee.Address = "New York";
+            return View(employee);
+        }
+    }
+}
---
+++ Fundamentals/AspNetCore.Views/AspNetCore.Views/Controllers/ExampleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace AspNetCore.Views.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ViewBagExample()
+        {
+            ViewBag.CurrentDateTime = DateTime.Now;
+            ViewBag.CurrentYear = DateTime.Now.Year;
+            return View();
+        }
+        public IActionResult TempDataExample()
+        {
+            TempData["CurrentDateTime"] = DateTime.Now;
+            TempData["CurrentYear"] = DateTime.Now.Year;
+            return RedirectToAction("TempDataShow");
+        }
+        public IActionResult TempDataShow()
+        {
+            return View();
+        }
+    }
+}
