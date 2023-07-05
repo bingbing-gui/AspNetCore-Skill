762d3177f5b115409ce3b375ea87df412451effb
bingbing.gui <bingbing@DESKTOP-K7ELFM9>
2023-07-05 18:03:10 +0900

    ASP.NET Core View

--- Fundamentals/AspNetCore.Views/AspNetCore.Views/Controllers/HomeController.cs
+++ Fundamentals/AspNetCore.Views/AspNetCore.Views/Controllers/HomeController.cs
@@ -1,49 +1,16 @@
-﻿using AspNetCore.Views.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCore.Views.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
-        {
-            //return RedirectToAction("Privacy");
-            return RedirectToActionPermanent("Privacy");
-        }
-        public RedirectResult RedirectAction()
-        {
-            return Redirect("/List/Search");
-        }
-        public RedirectResult RedirectPermanentAction()
-        {
-            return RedirectPermanent("/List/Search");
-        }
-        public RedirectToRouteResult Redirect()
-        {
-            return RedirectToRoute(new { controller = "Admin", action = "Users", ID = 10 });
-        }
-        public RedirectToRouteResult RedirectPermanent()
-        {
-            return RedirectToRoutePermanent(new { controller = "Admin", action = "Users", ID = 10 });
-        }
-        public IActionResult Privacy()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult CallSharedView()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
