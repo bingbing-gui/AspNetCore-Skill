33f51815b20d5d0aea2cef3f1f64d3ef19ae3060
bingbing.gui <bingbing@DESKTOP-K7ELFM9>
2023-07-28 17:46:22 +0900

    ASP.NET Core Area

---
+++ Fundamentals/AspNetCore.Route/AspNetCore.Areas/Areas/Sales/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using AspNetCore.Areas.Areas.Sales.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Areas.Areas.Sales.Controllers
+{
+    [Area("Sales")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product[] productArray = new Product[] {
+                new Product { Name = "Pants", Quantity = 5, Price=100 },
+                new Product { Name = "Shirts", Quantity = 10, Price=80 },
+                new Product { Name = "Shoes", Quantity = 15, Price=50 }
+            };
+            return View(productArray);
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ Fundamentals/AspNetCore.Route/AspNetCore.Areas/Areas/Sales/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCore.Areas.Areas.Sales.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
