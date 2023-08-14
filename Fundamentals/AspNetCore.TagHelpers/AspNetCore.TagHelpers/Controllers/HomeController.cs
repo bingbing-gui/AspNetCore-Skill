e2b4931144420e8e97c22490439c08cee8634fdb
bingbing.gui <bingbing@DESKTOP-K7ELFM9>
2023-08-14 15:50:03 +0900

    ASP.NET Core Tag Helper

--- Fundamentals/AspNetCore.TagHelpers/AspNetCore.TagHelpers/Controllers/HomeController.cs
+++ Fundamentals/AspNetCore.TagHelpers/AspNetCore.TagHelpers/Controllers/HomeController.cs
@@ -1,21 +1,25 @@
 ﻿using AspNetCore.TagHelpers.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-
+using static AspNetCore.TagHelpers.Models.Repository;
 namespace AspNetCore.TagHelpers.Controllers
 {
     public class HomeController : Controller
     {
+        private IRepository _repository;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IRepository repository,
+            ILogger<HomeController> logger)
         {
+            _repository = repository;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_repository.Products);
         }
 
         public IActionResult Privacy()
---
+++ Fundamentals/AspNetCore.TagHelpers/AspNetCore.TagHelpers/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCore.TagHelpers.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
