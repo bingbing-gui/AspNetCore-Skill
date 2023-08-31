eb779b8c459ce872f27b988880a0daca499f1fc3
bingbing.gui <bingbing@DESKTOP-K7ELFM9>
2023-08-31 09:03:41 +0900

    客户自定义标签

--- Fundamentals/AspNetCore.TagHelpers/AspNetCore.CustomTagHelpers/Controllers/HomeController.cs
+++ Fundamentals/AspNetCore.TagHelpers/AspNetCore.CustomTagHelpers/Controllers/HomeController.cs
@@ -7,12 +7,10 @@ namespace AspNetCore.CustomTagHelpers.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
             return View();
