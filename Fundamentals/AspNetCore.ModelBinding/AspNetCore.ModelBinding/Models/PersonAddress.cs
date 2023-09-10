ff991c1cf58da83ad245a9640fcbc1d00724fa63
bingbing gui <bingbing.gui@outlook.com>
2023-09-10 15:17:49 +0900

    ASP.NET Core ModelBinding

---
+++ Fundamentals/AspNetCore.ModelBinding/AspNetCore.ModelBinding/Models/PersonAddress.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+namespace AspNetCore.ModelBinding.Models
+{
+    public class PersonAddress
+    {
+        public string City { get; set; }
+        [BindNever]
+        public string Country { get; set; }
+    }
+}
