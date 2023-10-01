e05c4574537f0f6e924afc4980e119e25d8c05c3
bingbing <bingbing@DESKTOP-K7ELFM9>
2023-10-01 18:50:05 +0900

    模型验证验证

---
+++ Fundamentals/AspNetCore.ModelValidation/AspNetCore.ModelValidation/Infrastructure/CustomDate.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace AspNetCore.ModelValidation.Infrastructure
+{
+    public class CustomDate : Attribute, IModelValidator
+    {
+        public IEnumerable<ModelValidationResult> Validate(ModelValidationContext context)
+        {
+            if (Convert.ToDateTime(context.Model) > DateTime.Now)
+                return new List<ModelValidationResult> {
+                    new ModelValidationResult("", "日期不能大于当前日期")
+                };
+            else if (Convert.ToDateTime(context.Model) < new DateTime(1980, 1, 1))
+                return new List<ModelValidationResult> {
+                    new ModelValidationResult("", "日期不能再1980年以前")
+                };
+            else
+                return Enumerable.Empty<ModelValidationResult>();
+        }
+    }
+}
---
+++ Fundamentals/AspNetCore.ModelValidation/AspNetCore.ModelValidation/Infrastructure/NameValidate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace AspNetCore.ModelValidation.Infrastructure
+{
+    public class NameValidate : Attribute, IModelValidator
+    {
+        public string[] NotAllowed { get; set; }
+        public string ErrorMessage { get; set; }
+        public IEnumerable<ModelValidationResult> Validate(ModelValidationContext context)
+        {
+            if (NotAllowed.Contains(context.Model as string))
+                return new List<ModelValidationResult> {
+                    new ModelValidationResult("", ErrorMessage)
+                };
+            else
+                return Enumerable.Empty<ModelValidationResult>();
+        }
+    }
+}
