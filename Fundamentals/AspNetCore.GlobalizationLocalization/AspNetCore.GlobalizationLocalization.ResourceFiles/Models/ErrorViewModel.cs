d3364eadc2aeb8a5e02e16a69514b45b5646e796
bingbing <bingbing@DESKTOP-K7ELFM9>
2023-11-01 16:45:54 +0900

    ASP.NET Core 本地化和全球化

---
+++ Fundamentals/AspNetCore.GlobalizationLocalization/AspNetCore.GlobalizationLocalization.ResourceFiles/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCore.GlobalizationLocalization.ResourceFiles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
