76bb07b9c0b3aeaf735d9fd3917fac2d73af7d57
bingbing <bingbing@DESKTOP-K7ELFM9>
2023-12-03 18:20:29 +0900

    ASP.NET Core XSRF 防伪Token

--- Fundamentals/AspNetCore.Security/AspNetCore.XSRF/Program.cs
+++ Fundamentals/AspNetCore.Security/AspNetCore.XSRF/Program.cs
@@ -1,7 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+//全局示例 
+//可以使用 IgnoreAntiforgeryToken 筛选器，给指定的Action（或控制器）无需防伪造令牌
+builder.Services.AddControllersWithViews(options =>
+{
+    options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+});
+
+builder.Services.AddAntiforgery(options =>
+{
+    // Set Cookie properties using CookieBuilder properties†.
+    options.FormFieldName = "AntiforgeryFieldname";
+    options.HeaderName = "X-CSRF-TOKEN-HEADERNAME";
+    options.SuppressXFrameOptionsHeader = false;
+});
+
 
 var app = builder.Build();
 
