ef9f1b8772dbe2c5c4a63118c598170f5a476a93
bingbing <bingbing@DESKTOP-K7ELFM9>
2023-12-05 16:38:47 +0900

    Open Redirect Attacks

---
+++ Fundamentals/AspNetCore.Security/AspNetCore.OpenRedirectAttacks/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using AspNetCore.OpenRedirectAttacks.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AspNetCore.OpenRedirectAttacks.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SomeAction(string redirectUrl)
+        {
+            return LocalRedirect(redirectUrl);
+        }
+        private IActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
