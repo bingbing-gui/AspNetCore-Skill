548e61d37f50f09d24e9a321db7764b475d93edd
bingbing <bingbing@DESKTOP-K7ELFM9>
2023-12-05 17:21:30 +0900

    移除旧项目

--- Fundamentals/AspNetCore.Security/AspNetCore.Authentication/AspNetCore.IdentityJWT.API/Services/IMailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AspNetCore.IdentityJWT.API.Services
-{
-    public interface IMailService
-    {
-        Task SendEmailAsync(string toEmail, string subject, string content);
-    }
-}
--- Fundamentals/AspNetCore.Security/AspNetCore.UserSecrets/AspNetCore.UserSecrets/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AspNetCore.UserSecrets.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
