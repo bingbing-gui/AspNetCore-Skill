6b3617363a3f27b14c9d584e1407680e952e8d3a
bingbing <bingbing@DESKTOP-K7ELFM9>
2023-12-13 18:00:22 +0900

    AspNet Core XSS

---
+++ Fundamentals/AspNetCore.Security/AspNetCore.XSS/Controllers/BlogController.cs
@@ -0,0 +1,36 @@
+﻿using AspNetCore.XSS.Models;
+using AspNetCore.XSS.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.XSS.Controllers
+{
+    public class BlogController : Controller
+    {
+        private IBloggingRepository _repository;
+        public BlogController(IBloggingRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var blogs=await _repository.GetAllBlogs();
+            return View(blogs);
+        }
+        [HttpGet]
+        public IActionResult AddBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddBlog(Blog blog)
+        {
+            await _repository.AddBlog(blog);
+            return View();
+        }
+        [HttpGet]
+        public string GetProfile(string id)
+        {
+            return $"你好，我叫 {id}.";
+        }
+    }
+}
