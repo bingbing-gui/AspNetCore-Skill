7574c980cf85553e50811f96d45470ca04c8155a
bingbing <bingbing@DESKTOP-K7ELFM9>
2024-01-10 17:52:45 +0900

    调整解决方案中项目的目录结构

--- WebApps/MVC/MvcMovie/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MvcMovie.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- WebApps/PartialViews/PartialViews/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PartialViews.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
