bc2279674339cd30ab00343949ac4b97b24adf12
bingbing <bingbing@DESKTOP-K7ELFM9>
2024-01-11 15:00:40 +0900

    修改Swagger项目

--- Fundamentals/AspNetCore.Swagger/AspNetCore.Swashbuckle/Program.cs
+++ Fundamentals/AspNetCore.Swagger/AspNetCore.Swashbuckle/Program.cs
@@ -1,5 +1,6 @@
 using AspNetCore.Swashbuckle.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
 
@@ -14,39 +15,44 @@ builder.Services.AddDbContext<TodoContext>(options =>
     options.UseInMemoryDatabase("Todo"));
 
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
     {
-        Version = "v1",
-        Title = "ToDo API",
-        Description = "An ASP.NET Core Web API for managing ToDo items",
-        TermsOfService = new Uri("https://example.com/terms"),
-        Contact = new OpenApiContact
+        options.SwaggerDoc("v1", new OpenApiInfo
         {
-            Name = "Example Contact",
-            Url = new Uri("https://example.com/contact")
-        },
-        License = new OpenApiLicense
-        {
-            Name = "Example License",
-            Url = new Uri("https://example.com/license")
-        }
+            Version = "v1",
+            Title = "ToDo API",
+            Description = "An ASP.NET Core Web API for managing ToDo items",
+            TermsOfService = new Uri("https://example.com/terms"),
+            Contact = new OpenApiContact
+            {
+                Name = "Example Contact",
+                Url = new Uri("https://example.com/contact")
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Example License",
+                Url = new Uri("https://example.com/license")
+            }
+        });
     });
-
-    // using System.Reflection;
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
+//    // using System.Reflection;
+//var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+//options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(options =>
+    {
+        options.SerializeAsV2 = true;
+    });
+    app.UseSwaggerUI(options =>
+    {
+        //options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        //options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
