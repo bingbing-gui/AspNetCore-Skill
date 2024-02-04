d86194aff7e3af214ffef8ccfc3863d4f9b64a75
bingbing gui <bingbing.gui@outlook.com>
2024-02-04 15:10:51 +0900

    清理ASP.NET Core Options项目中旧代码

--- Fundamentals/AspNetCore.Options/AspNetCore.Options/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace AspNetCore.Options.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
