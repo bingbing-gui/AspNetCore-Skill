ac9c0d1ee791ce10833149c509b96b875227da92
bingbing gui <bingbing.gui@outlook.com>
2024-02-10 14:56:58 +0900

    AspNetCore.FormatResponseOutputData

--- Fundamentals/AspNetCore.APIControllers/AspNetCore.FormatResponseOutputData/Program.cs
+++ Fundamentals/AspNetCore.APIControllers/AspNetCore.FormatResponseOutputData/Program.cs
@@ -1,8 +1,22 @@
+﻿using AspNetCore.FormatResponseOutputData.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers()
+//                .AddXmlSerializerFormatters();
+//builder.Services.AddControllers()
+//                .AddJsonOptions(options =>
+//                {
+//                    //JSON 序列化器将使用 .NET 对象的属性名作为 JSON 属性名，而不会进行任何额外的命名转换
+//                    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+//                });
+
+builder.Services.AddControllers()
+    .AddNewtonsoftJson();
+
+builder.Services.AddSingleton<TodoItemStore>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
