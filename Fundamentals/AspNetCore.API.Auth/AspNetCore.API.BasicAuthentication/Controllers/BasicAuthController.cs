56b0f9dce93addbe92477a47a4f14a3eded7a0e6
bingbing gui <bingbing.gui@outlook.com>
2024-02-12 17:50:28 +0900

    添加AspNetCore.API.BasicAuthentication

---
+++ Fundamentals/AspNetCore.API.Auth/AspNetCore.API.BasicAuthentication/Controllers/BasicAuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AspNetCore.API.BasicAuthentication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BasicAuthController : ControllerBase
+    {
+        public BasicAuthController()
+        {
+
+        }
+        [Authorize]
+        [HttpGet("auth", Name = "BasicAuth")]
+        public ActionResult BasicAuth()
+        {
+            var response = new
+            {
+                Message = "授权成功",
+                ServerTime = DateTime.Now,
+            };
+            return Ok(response);
+        }
+    }
+}
