de8bd147290a91e2627267cc154e86da5fdaa037
bingbing gui <bingbing.gui@outlook.com>
2024-02-15 12:58:59 +0900

    ASP.NET Core JWT Authentication

---
+++ Fundamentals/AspNetCore.API.Auth/AspNetCore.API.JWT.Authentication/Controllers/JwtAuthController.cs
@@ -0,0 +1,94 @@
+﻿using AspNetCore.API.JWT.Authentication.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCore.API.JWT.Authentication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JwtAuthController : ControllerBase
+    {
+        public readonly User _user;
+        private readonly IConfiguration _configuration;
+        public JwtAuthController(IConfiguration configuration, User user)
+        {
+            _configuration = configuration;
+            _user = user;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            _user.Username = request.Username;
+            _user.PasswordHash = passwordHash;
+            _user.PasswordSalt = passwordSalt;
+
+            return Ok(_user);
+        }
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if (_user.Username != request.Username)
+            {
+                return BadRequest("User not found.");
+            }
+            if (!VerifyPasswordHash(request.Password, _user.PasswordHash, _user.PasswordSalt))
+            {
+                return BadRequest("Wrong password.");
+            }
+            string token = CreateToken(_user);
+            return Ok(token);
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+            var secret = _configuration.GetSection("JwtSetting:Secret").Value;
+            var issue = _configuration.GetSection("JwtSetting:Issuer").Value;
+            var audience = _configuration.GetSection("JwtSetting:Audience").Value;
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(secret));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                issuer: issue,
+                audience: audience,
+                claims: claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
---
+++ Fundamentals/AspNetCore.API.Auth/AspNetCore.API.JWT.Authentication/Model/User.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCore.API.JWT.Authentication.Model
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
