459b598fe2a9bde9da97605b21a78e62b938983e
bingbing gui <bingbing.gui@outlook.com>
2024-06-12 21:52:15 +0800

    Quantz定时任务处理

---
+++ src/AspNetCore.Third-Party.Library/AspNetCore.Quartz/Program.cs
@@ -0,0 +1,53 @@
+﻿using AspNetCore.Quartz.Models;
+using Quartz;
+using Quartz.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddQuartz(q =>
+{
+    // Just use the name of your job that you created in the Jobs folder.
+    var jobKey = new JobKey("SyncFinishedOrderJob");
+    q.AddJob<SyncFinishedOrderJob>(opts => opts.WithIdentity(jobKey));
+
+    q.AddTrigger(opts => opts
+        .ForJob(jobKey)
+        .WithIdentity("SyncFinishedOrderJob-trigger")
+        //每天凌晨3点运行一次
+        //.WithCronSchedule("0 0 3 * * ?")
+        .WithCronSchedule("0 * * ? * *")
+    );
+});
+
+// ASP.NET Core hosting
+builder.Services.AddQuartzServer(options =>
+{
+    // when shutting down we want jobs to complete gracefully
+    options.WaitForJobsToComplete = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
