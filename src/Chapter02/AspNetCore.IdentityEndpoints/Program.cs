1c7310ddc4db76ba858c8e987f2fafc10f17d4b6
bingbing gui <bingbing.gui@outlook.com>
2024-06-17 21:32:06 +0800

    Chapter02/AspNetCore IdentityEndpoints Chapter03

---
+++ src/Chapter02/AspNetCore.IdentityEndpoints/Program.cs
@@ -0,0 +1,42 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddAuthorizationBuilder();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("AppDb"));
+
+builder.Services.AddIdentityCore<MyUser>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddApiEndpoints();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Adds /register, /login and /refresh endpoints
+app.MapIdentityApi<MyUser>();
+
+app.MapGet("/", (ClaimsPrincipal user) => $"Hello {user.Identity!.Name}").RequireAuthorization();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
+
+class MyUser : IdentityUser { }
+
+class AppDbContext : IdentityDbContext<MyUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}
+
