7c4dfc3b5b6a6af4c95afdb9f12d5dd600c28890
bingbing gui <bingbing.gui@outlook.com>
2024-06-18 21:09:31 +0800

    Asp.Net Core Tag Helpers 入门

---
+++ src/Chapter14/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using AspNetCore.TagHelpers.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using static AspNetCore.TagHelpers.Models.Repository;
+namespace AspNetCore.TagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository _repository;
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(IRepository repository,
+            ILogger<HomeController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_repository.Products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/Chapter14/CustomTagHelpers/MyTable.cs
@@ -0,0 +1,29 @@
+﻿using AspNetCore.TagHelpers.Models;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+namespace AspNetCore.TagHelpers.CustomTagHelpers
+{
+    [HtmlTargetElement("MyTable")]
+    public class MyTable : TagHelper
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "table";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("class","table table-sm table-bordered");
+            StringBuilder sb = new StringBuilder();
+            foreach (var product in Products)
+                sb.Append("<tr><td>" + @product.Name + "</td><td>" + @product.Price
+                + "</td><td>" + @product.Quantity + "</td></tr>");
+            output.Content.SetHtmlContent($@"<thead class=""bg-dark text-white"">
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Price</th>
+                                                <th>Quantity</th>
+                                            </tr>
+                                        </thead>
+                                        {sb.ToString()}<tbody></tbody>");
+        }
+    }
+}
