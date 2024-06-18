0da095c8daa2e324a5d48237cf258b1b2d4b7f07
bingbing gui <bingbing.gui@outlook.com>
2024-06-18 21:29:42 +0800

    ASP.NET Core 全球化和本地化系列二 在ASP.NET Core 使用PO文件指定本地化

---
+++ src/Chapter22/Models/JobApplication.cs
@@ -0,0 +1,29 @@
+﻿using AspNetCore.GlobalLocalResFiles.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AspNetCore.GlobalLocalResFiles.Models
+{
+    public class JobApplication
+    {
+        [Required(ErrorMessage = "Please provide your name")]
+        [Display(Name = "Job applicant name")]
+        public string Name { get; set; }
+
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        [Display(Name = "Job applicant email")]
+        public string Email { get; set; }
+        [CustomDate]
+        [Display(Name = "Date of Birth")]
+        public DateTime DOB { get; set; }
+        [Required(ErrorMessage = "Please select your sex")]
+        [Display(Name = "Job applicant sex")]
+        public string Sex { get; set; }
+        [Range(2, 4, ErrorMessage = "{0} must be a number between {1} and {2}")]
+        [Display(Name = "Job applicant experience")]
+        public int Experience { get; set; }
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the Terms")]
+        [Display(Name = "Terms")]
+        public bool TermsAccepted { get; set; }
+    }
+}
