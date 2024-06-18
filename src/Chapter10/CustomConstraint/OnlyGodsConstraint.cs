10713fc524c6050a51d5121cc9550ad6d4f1147e
bingbing gui <bingbing.gui@outlook.com>
2024-06-18 20:55:11 +0800

    路由约束

---
+++ src/Chapter10/CustomConstraint/OnlyGodsConstraint.cs
@@ -0,0 +1,13 @@
+﻿namespace AspNetCore.RouteConstraint.CustomConstraint
+{
+    public class OnlyGodsConstraint : IRouteConstraint
+    {
+        private string[] gods = new[] { "Ram", "Shiv", "Krishn", "Vishnu", "Brahma","Lakshmi" };
+        public bool Match(HttpContext? httpContext, IRouter? route, 
+                          string routeKey, RouteValueDictionary values, 
+                          RouteDirection routeDirection)
+        {
+            return gods.Contains(values[routeKey]);
+        }
+    }
+}
