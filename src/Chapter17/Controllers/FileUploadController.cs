d381d23c1e81b35569c4def69bcf94afeb742139
bingbing gui <bingbing.gui@outlook.com>
2024-06-18 21:15:14 +0800

    模型绑定

---
+++ src/Chapter17/Controllers/FileUploadController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.ModelBinding.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        private IWebHostEnvironment hostingEnvironment;
+        public FileUploadController(IWebHostEnvironment environment)
+        {
+            hostingEnvironment = environment;
+        }
+        public IActionResult Index() => View();
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            string path = Path.Combine(hostingEnvironment.WebRootPath, "Images/" + file.FileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return View((object)"Success");
+        }
+    }
+}
