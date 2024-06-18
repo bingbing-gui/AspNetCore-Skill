ad219e4e97285a1f8848980ea0d22a0a67523339
bingbing gui <bingbing.gui@outlook.com>
2024-06-18 21:07:00 +0800

    ASP.NET Core Areas

---
+++ src/Chapter13/Areas/Employee/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Areas.Areas.Employee.Controllers
+{
+    [Area("Employee")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
