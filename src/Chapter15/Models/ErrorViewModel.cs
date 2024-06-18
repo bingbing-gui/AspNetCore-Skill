f0e62ea7fc54ad86117ce5218a47bbcae635b9df
bingbing gui <bingbing.gui@outlook.com>
2024-06-18 21:11:15 +0800

    ASP.NET Core 内置的Tag Helpers

---
+++ src/Chapter15/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCore.BuiltInTagHelpers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
