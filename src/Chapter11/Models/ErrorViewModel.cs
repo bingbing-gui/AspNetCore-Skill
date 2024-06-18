48c76d6b623b00402bac2b3e06d3eab3b5d57e25
bingbing gui <bingbing.gui@outlook.com>
2024-06-18 20:58:32 +0800

    ASP.NET Core Attribute 路由

---
+++ src/Chapter11/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCore.AttributeRoute.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
