96fb1736e3d8106c795306aa0e56c9b144c3c771
bingbing gui <bingbing.gui@outlook.com>
2024-06-18 22:02:30 +0800

    ASP.NET Core Cookie

---
+++ src/Chpater25/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCore.Cookie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
