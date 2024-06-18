f7a41783918eee2f6f2da06dcb87a66d14b01845
bingbing gui <bingbing.gui@outlook.com>
2024-06-18 20:40:20 +0800

    ASP.NET Core 依赖注入

---
+++ src/Chapter05/Models/ProductionRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace AspNetCore.DependencyInjection.Models
+{
+    public class ProductionRepository : IRepository
+    {
+        private Dictionary<string, Product> products;
+        public ProductionRepository()
+        {
+            products = new Dictionary<string, Product>();
+            new List<Product> {
+                new Product { Name = "Women Shoes", Price = 99M },
+                new Product { Name = "Skirts", Price = 29.99M },
+                new Product { Name = "Pants", Price = 40.5M }
+            }.ForEach(p => AddProduct(p));
+        }
+        public IEnumerable<Product> Products => products.Values;
+        public Product this[string name] => products[name];
+        public void AddProduct(Product product) => products[product.Name] = product;
+        public void DeleteProduct(Product product) => products.Remove(product.Name);
+    }
+}
---
+++ src/Chapter05/Models/Repository.cs
@@ -0,0 +1,28 @@
+﻿namespace AspNetCore.DependencyInjection.Models
+{
+    public class Repository : IRepository
+    {
+        private IStorage _storage;
+        public Repository(IStorage storage)
+        {
+            _storage = storage;
+            new List<Product> {
+                new Product { Name = "Women Shoes", Price = 99M },
+                new Product { Name = "Skirts", Price = 29.99M },
+                new Product { Name = "Pants", Price = 40.5M }
+            }.ForEach(p => AddProduct(p));
+
+        }
+
+        public IEnumerable<Product> Products => _storage.Items;
+        public Product this[string name] => _storage[name];
+        public void AddProduct(Product product) => _storage[product.Name] = product;
+        public void DeleteProduct(Product product) => _storage.RemoveItem(product.Name);
+
+        private string guid = Guid.NewGuid().ToString();
+        public override string ToString()
+        {
+            return guid;
+        }
+    }
+}
