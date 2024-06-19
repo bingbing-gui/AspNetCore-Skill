47a63f43d46ab4c57b6e347ae2ef5b05884b0f2f
bingbing gui <bingbing.gui@outlook.com>
2024-06-19 20:17:18 +0800

    EFCoreMigration

---
+++ src/Chapter41/Models/CompanyContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreMigration.Models
+{
+    public class CompanyContext : DbContext
+    {
+        public CompanyContext(DbContextOptions<CompanyContext> options)
+                      : base(options)
+        {
+        }
+        public DbSet<Client> Clients { get; set; }
+       
+    }
+}
