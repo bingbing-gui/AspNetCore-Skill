529db2fed973b67c4d4fc5a046a0edaa4d213c25
bingbing gui <bingbing.gui@outlook.com>
2024-06-19 20:49:14 +0800

    EFCoreFluentAPIOneToMany

---
+++ src/Chapter48/Models/City.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreFluentAPIOneToMany.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int FKCountry { get; set; }
+        public Country Country { get; set; }
+    }
+}
---
+++ src/Chapter48/Program.cs
@@ -0,0 +1,34 @@
+using EFCoreFluentAPIOneToMany.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<CountryContext>(option =>
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
