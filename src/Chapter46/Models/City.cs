83f2f0b114f533132d77e432756f11b1351e479e
bingbing gui <bingbing.gui@outlook.com>
2024-06-19 20:35:10 +0800

    EFCoreConfiguration

---
+++ src/Chapter46/Models/City.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreConfiguration.Models
+{
+    [Table("TBL_City")]
+    public class City
+    {
+        [Key]
+        public int KeyId { get; set; }
+        [Column("CityName", TypeName = "varchar(25)")]
+        public string Name { get; set; }
+        [NotMapped]
+        public int Population { get; set; }
+        [ForeignKey("FKid")]
+        public Country Country { get; set; }
+    }
+}
