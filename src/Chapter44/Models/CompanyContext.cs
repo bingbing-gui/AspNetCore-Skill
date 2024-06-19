5682675012e955365103ba6221872371d49ad323
bingbing gui <bingbing.gui@outlook.com>
2024-06-19 20:27:09 +0800

    Entity Framework Core 更新数据

---
+++ src/Chapter44/Models/CompanyContext.cs
@@ -0,0 +1,28 @@
+﻿using EFCoreUpdateRecords.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreReadRecords.Models
+{
+    public class CompanyContext : DbContext
+    {
+        public CompanyContext(DbContextOptions<CompanyContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.HasOne(d => d.Department)
+                    .WithMany(p => p.Employee)
+                    .HasForeignKey(d => d.DepartmentId)
+                    .OnDelete(DeleteBehavior.Cascade)
+                    .HasConstraintName("FK_Employee_Department");
+            });
+        }
+    }
+}
---
+++ src/Chapter44/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreUpdateRecords.Models
+{
+    public class Employee
+    {
+        [DisplayName("编号")]
+        public int Id { get; set; }
+        [DisplayName("部门")]
+        public int DepartmentId { get; set; }
+        [DisplayName("姓名")]
+        public string Name { get; set; }
+        [DisplayName("职务")]
+        public string Designation { get; set; }
+        [DisplayName("部门")]
+        public Department Department { get; set; }
+    }
+}
