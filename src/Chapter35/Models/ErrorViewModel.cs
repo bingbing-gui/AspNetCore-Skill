a4219250562dea1c040b77ae485ab995eba9ec27
bingbing gui <bingbing.gui@outlook.com>
2024-06-19 19:14:03 +0800

    ASP.NET Core HttpClient组件拓展系列三

---
+++ src/Chapter35/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCore.HttpClientHander.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
