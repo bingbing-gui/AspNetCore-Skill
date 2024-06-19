cc42a3af635c1d567211c9a8ee9d763d56ed006c
bingbing gui <bingbing.gui@outlook.com>
2024-06-19 20:05:21 +0800

    Entity Framework Core DbContext

---
+++ src/Chapter39/Migrations/20230411095223_migrations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCoreDbContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class migrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
