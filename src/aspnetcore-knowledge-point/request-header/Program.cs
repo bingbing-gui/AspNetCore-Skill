60396ccc7902745262c8ffbcd06c4c3bf6051428
bingbing gui <bingbing.gui@outlook.com>
2024-10-13 15:22:42 +0900

    Http 头

---
+++ src/aspnetcore-knowledge-point/request-header/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.Net.Http.Headers;
+using System.Reflection;
+
+List<FieldInfo> GetConstants(Type type)
+{
+    FieldInfo[] fieldInfos = type.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+
+    return fieldInfos.ToList();
+}
+
+var app = WebApplication.Create();
+app.Run(async context =>
+{
+    context.Response.Headers.Append("content-type", "text/html");
+    await context.Response.WriteAsync("<h1>Request Headers</h1>");
+    await context.Response.WriteAsync("<ul>");
+    foreach (var h in context.Request.Headers)
+    {
+        await context.Response.WriteAsync($"<li>{h.Key} : {h.Value}</li>");
+    }
+    await context.Response.WriteAsync("</ul>");
+
+    await context.Response.WriteAsync("<h1>Request Headers from Microsoft.Net.Http.Headers.HeaderNames</h1>");
+    await context.Response.WriteAsync("<ul>");
+    foreach (var h in GetConstants(typeof(HeaderNames)))
+    {
+        await context.Response.WriteAsync($"<li>{h.Name} = {h.GetValue(h)}</li>");
+    }
+    await context.Response.WriteAsync("</ul>");
+
+
+});
+app.Run();
