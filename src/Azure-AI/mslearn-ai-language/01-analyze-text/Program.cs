75ed263137a9a0ec3a178a22cf540c712c4d66d5
bingbing gui <bingbing.gui@outlook.com>
2024-11-23 19:08:32 +0900

    http-response

--- src/Azure-AI/mslearn-ai-language/01-analyze-text/Program.cs
+++ src/Azure-AI/mslearn-ai-language/01-analyze-text/Program.cs
@@ -2,10 +2,9 @@
 using System.IO;
 using System.Text;
 using Microsoft.Extensions.Configuration;
-
-// 添加命名空间
 using Azure;
 using Azure.AI.TextAnalytics;
+
 
 namespace text_analysis
 {
@@ -16,7 +15,6 @@ namespace text_analysis
             try
             {
                 Console.OutputEncoding = Encoding.UTF8;
-
                 // Get config settings from AppSettings
                 IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
                 IConfigurationRoot configuration = builder.Build();
@@ -27,32 +25,32 @@ namespace text_analysis
                 AzureKeyCredential credentials = new AzureKeyCredential(aiSvcKey);
                 Uri endpoint = new Uri(aiSvcEndpoint);
                 TextAnalyticsClient aiClient = new TextAnalyticsClient(endpoint, credentials);
+
                 // Analyze each text file in the reviews folder
-                var folderPath = Path.GetFullPath("./reviews");  
+                var folderPath = Path.GetFullPath("./reviews");
                 DirectoryInfo folder = new DirectoryInfo(folderPath);
                 foreach (var file in folder.GetFiles("*.txt"))
                 {
                     // Read the file contents
-                   // Console.WriteLine("\n-------------\n" + file.Name);
+                    Console.WriteLine("\n-------------\n" + file.Name);
                     StreamReader sr = file.OpenText();
                     var text = sr.ReadToEnd();
                     sr.Close();
-                    Console.WriteLine( text);
+                    Console.WriteLine("\n" + text);
 
                     // Get language
                     DetectedLanguage detectedLanguage = aiClient.DetectLanguage(text);
-                    Console.WriteLine($"语言: {detectedLanguage.Name}");
-
+                    Console.WriteLine($"\nLanguage: {detectedLanguage.Name}");
 
                     // Get sentiment
                     DocumentSentiment sentimentAnalysis = aiClient.AnalyzeSentiment(text);
-                    Console.WriteLine($"情感: {sentimentAnalysis.Sentiment}");
-                   
+                    Console.WriteLine($"\nSentiment: {sentimentAnalysis.Sentiment}");
+
                     // Get key phrases
                     KeyPhraseCollection phrases = aiClient.ExtractKeyPhrases(text);
                     if (phrases.Count > 0)
                     {
-                        Console.WriteLine("关键词:");
+                        Console.WriteLine("\nKey Phrases:");
                         foreach (string phrase in phrases)
                         {
                             Console.WriteLine($"\t{phrase}");
@@ -62,34 +60,28 @@ namespace text_analysis
                     CategorizedEntityCollection entities = aiClient.RecognizeEntities(text);
                     if (entities.Count > 0)
                     {
-                        Console.WriteLine("实体:");
+                        Console.WriteLine("\nEntities:");
                         foreach (CategorizedEntity entity in entities)
                         {
                             Console.WriteLine($"\t{entity.Text} ({entity.Category})");
                         }
                     }
-
                     // Get linked entities
                     LinkedEntityCollection linkedEntities = aiClient.RecognizeLinkedEntities(text);
                     if (linkedEntities.Count > 0)
                     {
-                        Console.WriteLine("连接:");
+                        Console.WriteLine("\nLinks:");
                         foreach (LinkedEntity linkedEntity in linkedEntities)
                         {
                             Console.WriteLine($"\t{linkedEntity.Name} ({linkedEntity.Url})");
                         }
                     }
-
                 }
-                Console.ReadLine();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
         }
-
-
-
     }
 }
---
+++ src/aspnetcore-knowledge-point/http-response/Program.cs
@@ -0,0 +1,19 @@
+﻿var app = WebApplication.Create();
+app.Run(async context =>
+{
+    context.Response.Headers.Append("content-type", "text/html;;charset=utf-8");
+    await context.Response.WriteAsync("<b>Hello world</b>");
+    try
+    {
+        if (!context.Response.HasStarted)
+        {
+            context.Response.Headers.Append("X-USER", "Bill");
+        }
+    }
+    catch (Exception ex)
+    {
+        await context.Response.WriteAsync($"<br/><br/>你不能修改Header集合，Body已经发送. Exception: {ex.Message}");
+    }
+});
+
+app.Run();
