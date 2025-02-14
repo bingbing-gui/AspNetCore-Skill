7efc98245a6958feec4de534bfc6fa9115f41c36
bingbing gui <bingbing.gui@outlook.com>
2025-02-14 17:34:30 +0900

    passwordhasher

---
+++ src/aspnetcore-knowledge-point/passwordhaser/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+var app = WebApplication.Create();
+
+app.Run(context =>
+{
+    var password = context.Request.Query["password"];
+
+    if (string.IsNullOrWhiteSpace(password))
+        password = "123456";
+
+    var usr = new User();
+    var hasher = new PasswordHasher<User>();
+    var hashedPassword = hasher.HashPassword(usr, password);
+
+    var isPasswordMatch = hasher.VerifyHashedPassword(usr, hashedPassword, password);
+
+    return context.Response.WriteAsync($"Password : {password} => Hashed : {hashedPassword} \nPassword Matched : {isPasswordMatch}");
+});
+
+app.Run();
+
+public record User();
+
+
+
