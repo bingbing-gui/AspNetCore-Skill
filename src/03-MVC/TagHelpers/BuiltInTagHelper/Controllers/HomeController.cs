c2851420775f2f355ac544b9668c59c9dfd4b3f4
bingbing gui <bingbing.gui@outlook.com>
2025-03-21 16:13:58 +0900

    重构目录、并升级到.NET9

---
+++ src/03-MVC/TagHelpers/BuiltInTagHelper/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using AspNetCore.BuiltInTagHelpers.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+using static AspNetCore.BuiltInTagHelpers.Models.Repository;
+
+namespace AspNetCore.BuiltInTagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IRepository _repository;
+        public HomeController(IRepository repository,
+                              ILogger<HomeController> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+        public ViewResult Edit()
+        {
+            ViewBag.Quantity = new SelectList(_repository.Products.Select(p => p.Quantity).Distinct());
+            return View("Create", _repository.Products.Last());
+        }
+        public IActionResult Index()
+        {
+            return View(_repository.Products);
+        }
+        public IActionResult Create()
+        {
+            ViewBag.Quantity = new SelectList(_repository.Products.Select(p => p.Quantity).Distinct());
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            _repository.AddProduct(product);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
